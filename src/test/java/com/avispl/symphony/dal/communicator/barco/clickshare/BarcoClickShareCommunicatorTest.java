437ec25baa7e833c545911e157f4e33af670568f
Maksym Rossiitsev <maksym.rossiitsev@avispl.com>
2020-07-20 21:33:06 +0300

    Test added

--- src/test/java/com/avispl/symphony/dal/communicator/barco/clickshare/BarcoClickShareCommunicatorTest.java
+++ src/test/java/com/avispl/symphony/dal/communicator/barco/clickshare/BarcoClickShareCommunicatorTest.java
@@ -1,7 +1,12 @@
 package com.avispl.symphony.dal.communicator.barco.clickshare;
 
+import com.avispl.symphony.api.dal.dto.monitor.ExtendedStatistics;
+import com.avispl.symphony.api.dal.dto.monitor.Statistics;
+import org.junit.Assert;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import java.util.List;
 
 public class BarcoClickShareCommunicatorTest {
     private static final String host = "***REMOVED***";
@@ -14,6 +19,7 @@ public class BarcoClickShareCommunicatorTest {
     @BeforeEach
     public void setUp() throws Exception {
         communicator = new BarcoClickShareCommunicator();
+        communicator.setTrustAllCertificates(true);
         communicator.setHost(host);
         communicator.setPort(port);
         communicator.setLogin(login);
@@ -24,6 +30,15 @@ public class BarcoClickShareCommunicatorTest {
 
     @Test
     public void testStatistics() throws Exception {
-        communicator.getMultipleStatistics();
+        List<Statistics> statistics = communicator.getMultipleStatistics();
+        Assert.assertNotNull(statistics.get(0));
+        Assert.assertEquals(1, statistics.size());
+        Assert.assertEquals("***REMOVED***", ((ExtendedStatistics)statistics.get(0)).getStatistics().get("Network wired#Configuration 1 Ip Address"));
+        Assert.assertEquals("HDMI", ((ExtendedStatistics)statistics.get(0)).getStatistics().get("Audio Output"));
+        Assert.assertEquals("ClickShare-1863550376", ((ExtendedStatistics)statistics.get(0)).getStatistics().get("Personalization#Meeting room name"));
+        Assert.assertEquals("00:04:A5:01:04:78", ((ExtendedStatistics)statistics.get(0)).getStatistics().get("Network wired#Configuration 1 MAC Address"));
+        Assert.assertEquals("R9861522EU", ((ExtendedStatistics)statistics.get(0)).getStatistics().get("Device information#Article number"));
+        Assert.assertEquals("ClickShare-1863550376", ((ExtendedStatistics)statistics.get(0)).getStatistics().get("Network#Hostname"));
+        Assert.assertEquals(14, ((ExtendedStatistics)statistics.get(0)).getControllableProperties().size());
     }
 }
