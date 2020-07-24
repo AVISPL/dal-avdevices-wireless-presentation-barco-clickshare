970cd981b7087476b2fce1f1cc0c0fc13289b285
Maksym Rossiitsev <maksym.rossiitsev@avispl.com>
2020-07-24 15:44:04 +0300

    extra trailing slash removed from urls

--- src/main/java/com/avispl/symphony/dal/communicator/barco/clickshare/BarcoClickShareProperties.java
+++ src/main/java/com/avispl/symphony/dal/communicator/barco/clickshare/BarcoClickShareProperties.java
@@ -4,19 +4,19 @@
 package com.avispl.symphony.dal.communicator.barco.clickshare;
 
 final class BarcoClickShareProperties {
-    public static final String POWER_MANAGEMENT = "/configuration/system/power-management";
-    public static final String OPERATIONS_REBOOT = "/operations/reboot";
-    public static final String OPERATIONS_STANDBY = "/operations/standby";
-    public static final String OPERATIONS_SUPPORTED = "/operations/supported";
-    public static final String CONFIGURATION_VIDEO = "/configuration/video";
-    public static final String CONFIGURATION_AUDIO = "/configuration/audio";
-    public static final String CONFIGURATION_NETWORK = "/configuration/system/network";
-    public static final String DEVICE_IDENTITY = "/configuration/system/device-identity";
-    public static final String PERSONALIZATION = "/configuration/personalization";
-    public static final String FEATURES_MIRACAST = "/configuration/features/miracast";
-    public static final String FEATURES_GOOGLECAST = "/configuration/features/google-cast";
-    public static final String FEATURES_BLACKBOARD = "/configuration/features/blackboard";
-    public static final String FEATURES_AIRPLAY = "/configuration/features/airplay";
+    public static final String POWER_MANAGEMENT = "configuration/system/power-management";
+    public static final String OPERATIONS_REBOOT = "operations/reboot";
+    public static final String OPERATIONS_STANDBY = "operations/standby";
+    public static final String OPERATIONS_SUPPORTED = "operations/supported";
+    public static final String CONFIGURATION_VIDEO = "configuration/video";
+    public static final String CONFIGURATION_AUDIO = "configuration/audio";
+    public static final String CONFIGURATION_NETWORK = "configuration/system/network";
+    public static final String DEVICE_IDENTITY = "configuration/system/device-identity";
+    public static final String PERSONALIZATION = "configuration/personalization";
+    public static final String FEATURES_MIRACAST = "configuration/features/miracast";
+    public static final String FEATURES_GOOGLECAST = "configuration/features/google-cast";
+    public static final String FEATURES_BLACKBOARD = "configuration/features/blackboard";
+    public static final String FEATURES_AIRPLAY = "configuration/features/airplay";
 //    public static final String CONFIGURATOR = "//configuration/configurator";
 //    public static final String FACTORY_RESET = "/configuration/troubleshooting/factory-reset";
 
